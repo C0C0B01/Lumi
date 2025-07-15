a01980583c021c73a732fc25aec5794956378555
cocobo1 <CocoboTheOne@gmail.com>
2025-07-15 18:19:49 +0300

    fix

--- src/plugins/dashless/index.ts
+++ src/plugins/dashless/index.ts
@@ -1,34 +1,17 @@
 import { ReactNative } from "src/metro/common/libraries/index.ts";
 import { after } from "src/patcher/index.ts";
-import { definePlugin, definePluginSettings, logger } from "#plugin-context";
+import { definePlugin } from "#plugin-context";
 import { Dev } from "@data/constants";
-
-export default definePlugin({
-  name: "Dashless",
-  description: "Changes dashes in text channel names to spaces",
-  authors: [Dev.cocobo1,
-  {
-    "name": "Awesomegamergame",
-    "id": 504401951623086081n
-  }],
-});
 
 const { View } = ReactNative;
 
-const log = (data?: any) => {
-  logger.info(data);
-};
-
-// Function to recursively traverse and modify children
 const traverseAndModify = (node: any): any => {
   if (typeof node === "string") {
     return node.replace(/-/g, " ");
   }
-
   if (Array.isArray(node)) {
     return node.map(traverseAndModify);
   }
-
   if (node && typeof node === "object" && node.props && node.props.children) {
     return {
       ...node,
@@ -38,24 +21,28 @@ const traverseAndModify = (node: any): any => {
       },
     };
   }
-
   return node;
 };
 
-const patchRender = () => {
-  return after(View, "render", (_, res) => {
-    //log("View render patched");
+let unpatchRender: () => void;
 
-    const modifiedTree = traverseAndModify(res);
+export default definePlugin({
+  name: "Dashless",
+  description: "Changes dashes in text channel names to spaces",
+  authors: [Dev.cocobo1,
+  {
+    "name": "Awesomegamergame",
+    "id": 504401951623086081n
+  }],
+  start() {
+    unpatchRender = after(View, "render", (_, res) => {
+      return traverseAndModify(res);
+    });
+  },
 
-    //log("React tree modified");
+  cleanup() {
+    unpatchRender?.();
+  }
+});
 
-    return modifiedTree;
-  });
-};
 
-const unpatchRender = patchRender();
-
-export const onUnload = () => {
-  unpatchRender();
-};
--- src/plugins/freecons/index.ts
+++ src/plugins/freecons/index.ts
@@ -1,46 +1,46 @@
-//import { byProps } from "@metro/common/filters";
-//import { Dev } from "@data/constants";
-//import { definePlugin, patcher } from "#plugin-context";
-//
-//const icons = byProps(["getOfficialAlternateIcons"])
-//const iconsIds = byProps(["FreemiumAppIconIds"])
+import { byProps } from "@metro/common/filters";
+import { Dev } from "@data/constants";
+import { definePlugin, patcher } from "#plugin-context";
+
+const icons = byProps(["getOfficialAlternateIcons"])
+const iconsIds = byProps(["FreemiumAppIconIds"])
 //const FreemiumAppIcons = iconsIds.FreemiumAppIconIds
 //let alternateIcons = icons.getOfficialAlternateIcons()
 //let mainIcons = icons.getIcons()
-//
-//export default definePlugin({
-//    name: "Free-App-Icons",
-//    description: "Allows for nitro app icons to be used, WIP",
-//    authors: [Dev.cocobo1,
-//    {
-//        name: "Dziurwa💕",
-//        id: 1001086404203389018n
-//    }
-//    ],
-//
-//    patches: [
-//        {
-//            id: "icons",
-//            target: byProps(["getIcons"]),
-//            patch(module, patcher) {
-//                patcher.instead(module, "getIcons", () => mainIcons);
-//            },
-//        },
-//        {
-//            id: "iconsIds",
-//            target: byProps(["getOfficialAlternateIcons"]),
-//            patch(module, patcher) {
-//                patcher.instead(module, "getOfficialAlternateIcons", () => alternateIcons);
-//            },
-//        },
-//        {
-//            id: "getIconById",
-//            target: byProps(["getIconById"]),
-//            patch(module, patcher) {
-//                patcher.after(module, "getIconById", (_, ret) => {
-//                    ret.isPremium = false;
-//                });
-//            },
-//        },
-//    ],
-//});
+
+export default definePlugin({
+    name: "Free-App-Icons",
+    description: "Allows for nitro app icons to be used, WIP",
+    authors: [Dev.cocobo1,
+    {
+        name: "Dziurwa💕",
+        id: 1001086404203389018n
+    }
+    ],
+
+    //patches: [
+    //    {
+    //        id: "icons",
+    //        target: byProps(["getIcons"]),
+    //        patch(module, patcher) {
+    //            patcher.instead(module, "getIcons", () => mainIcons);
+    //        },
+    //    },
+    //    {
+    //        id: "iconsIds",
+    //        target: byProps(["getOfficialAlternateIcons"]),
+    //        patch(module, patcher) {
+    //            patcher.instead(module, "getOfficialAlternateIcons", () => alternateIcons);
+    //        },
+    //    },
+    //    {
+    //        id: "getIconById",
+    //        target: byProps(["getIconById"]),
+    //        patch(module, patcher) {
+    //            patcher.after(module, "getIconById", (_, ret) => {
+    //                ret.isPremium = false;
+    //            });
+    //        },
+    //    },
+    //],
+});
